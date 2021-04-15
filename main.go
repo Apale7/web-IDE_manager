4bfc81fa8073084281a569e74d580ccc86bb3b38
Apale7 <1092377056@qq.com>
2021-04-15 21:34:03 +0800

    config port by env

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	port string
+)
+
+func init() {
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+	port = os.Getenv("web_ide_manager")
+	if port == "" {
+		port = "3070"
+	}
+}
+
 func main() {
 	logrus.SetReportCaller(true)
 	r := gin.Default()
-	defer r.Run(":3070")
+	defer r.Run(":" + port)
 	collectRoutes(r)
 }
